7bffdd46e9e21661ffea2602384860f0844c2314
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-07-17 18:18:59 +0900

    Add dmn file for si data

--- test/data/si/run.sh
+++ test/data/si/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#SBATCH --partition=G1
+#SBATCH --partition=G4
 #SBATCH --time=24:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=16
 #SBATCH --job-name=pos_si
 #SBATCH --output="error.%x"
 QE=/home/jmlim/dipole/q-e/bin
@@ -13,9 +13,9 @@ POSTW90=/home/jmlim/position/wannier90/postw90.x
 PREFIX=si
 
 # run parallel
-#srun -n $SLURM_NTASKS $QE/pw.x -nk 10 -in scf.in > scf.out
-#srun -n $SLURM_NTASKS $QE/pw.x -nk 10 -in bands.in > bands.out
-#srun -n $SLURM_NTASKS $QE/pw.x -nk 10 -in nscf.in > nscf.out
-#srun -n 1 $W90 -pp si
-#srun -n $SLURM_NTASKS $QE/pw2wannier90.x -nk 1 -pd .true. -in pw2wan.in > pw2wan.out
-srun -n $SLURM_NTASKS $W90 si
+srun -n 20 $QE/pw.x -nk 10 -in scf.in > scf.out
+srun -n 20 $QE/pw.x -nk 10 -in bands.in > bands.out
+srun -n 20 $QE/pw.x -nk 10 -in nscf.in > nscf.out
+srun -n 1 $W90 -pp si
+srun -n 20 $QE/pw2wannier90.x -nk 1 -pd .true. -in pw2wan.in > pw2wan.out
+srun -n 20 $W90 si
